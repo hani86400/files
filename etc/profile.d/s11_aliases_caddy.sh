eba4a6793cef3ba47364c0d5f39aca64812e98f3
hani86400 <hani86400@any.com>
2025-12-20 19:08:22 +0300

    25/12/20_19:08

--- etc/profile.d/s11_aliases_caddy.sh
+++ etc/profile.d/s11_aliases_caddy.sh
@@ -6,9 +6,24 @@ alias caddy_M=' sudo systemctl  mask    caddy_n8n.service'
 alias caddy_D=' sudo systemctl  disable caddy_n8n.service'
 alias caddy_E=' sudo systemctl  enable  caddy_n8n.service'
 
-alias caddy_J=' sudo journalctl -xeu    caddy_n8n.service -no-pager  -n 40'
+alias caddy_J=' sudo journalctl -xeu    caddy_n8n.service --no-pager  -n 40'
 
 alias caddy_S=' sudo systemctl  status  caddy_n8n.service'
 alias caddy_SS=' caddy_S ; ps aux | grep caddy'
 
 alias find_caddy='sudo find /etc/caddy /var/lib/caddy /var/log/caddy ; ls -l /usr/bin/caddy'
+###############################################################
+alias nginx_B=' sudo systemctl  start   nginx.service ; nginx_SS'
+alias nginx_K=' sudo systemctl  stop    nginx.service ; nginx_SS'
+alias nginx_R=' sudo systemctl  restart nginx.service ; nginx_SS'
+
+alias nginx_M=' sudo systemctl  mask    nginx.service'
+alias nginx_D=' sudo systemctl  disable nginx.service'
+alias nginx_E=' sudo systemctl  enable  nginx.service'
+
+alias nginx_J=' sudo journalctl -xeu    nginx.service --no-pager  -n 40'
+
+alias nginx_S=' sudo systemctl  status  nginx.service'
+alias nginx_SS=' nginx_S ; ps aux | grep nginx'
+
+alias find_nginx='sudo ls -lR /etc/nginx /var/log/nginx/ ; sudo nginx -t '
